12cd5f09f0b7baed1be2bef298989f79216cc64a
Elliot <etcclark@gmail.com>
2018-06-10 23:46:34 -0700

    .

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route } from 'react-router-dom'
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 import Search from './search'
+import Web from './page'
 
 class BooksApp extends React.Component {
   state = {
@@ -13,34 +14,15 @@ class BooksApp extends React.Component {
      * we're on, use the URL in the browser's address bar. This will ensure that
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
+     
+     <Route path="/search" component={Search}/>
+     
      */
   }
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -200,12 +182,10 @@ class BooksApp extends React.Component {
                 className="open-search"
                 to="/search"
                 ><a>Add a book</a>
-              </Link>
-              <Route path="/search" component={Search}/>
-          </div>
+              </Link>  
+            </div>
         )}
       </div>
-      
     )
   }
 }
